08513ed098511a12108a92a39f934055ab338f1b
Hellen Gakuruh <hellen.gakuruh@gmail.com>
2016-02-05 07:37:51 +0300

    Adding R Scripts

---
+++ Scripts/Exercises_Level_One.R
@@ -0,0 +1,98 @@
+# Exercises Level One
+
+#1. Know R and RStudio
+    #a) True or False, R is free 
+    #b) What is the difference between R and RStudio?
+    #c) What are the key benefits of working with RStudio?
+
+
+#2. Download and install Base R and RStudio
+    #a)
+    #b)
+
+
+#3. Be able to interact with R 
+    
+#4. Know, set and change the working directory
+    #a) Create a folder called "Exercises" and set your working directory to that directory
+    #b) 
+
+#5. Install and load packages
+    #a) What are packages and where can you get them?
+    #b) How would you install a package in R?
+    #c) What do you need to do, before using any package in R? 
+    #d) What function can you use to pull up the documentation on an installed package?
+    #e) Do you need to download the package called "Lattice"?
+    #f) Install the package called "ggplot2" and answer the following
+        # What is the package for? (Tip, look for the description file from it's documentation)
+                
+
+# Access the dataset called "Diamonds" from the package. How many variables does it have?
+
+
+
+
+#6. Interactively work with R's console
+
+
+#7. Learn how to write scripts in R
+
+
+#8. Make function calls
+
+
+#9. Know data types and objects used in R
+##Exercises on creating objects**
+    
+    #a. Vectors
+
+
+    #b. Matrices
+
+    #c. Data frames
+
+    #d. Lists
+
+#10.How to import data into R
+
+
+#11.Produce plots
+
+
+#12.Know how and where to get help
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
